a623398dd9a7167dae65e813cb42706f26223f87
eden299 <eden950310@gmail.com>
2026-07-02 16:17:51 +0800

    Add 0702 Java practice

---
+++ 0702/HelloJava.java
@@ -0,0 +1,6 @@
+public class HelloJava {public static void main(String[] args) {
+        System.out.println("Hello Java My name is your_name\r\n" + //
+                        "This is my Java practice.");
+    }
+    
+}
