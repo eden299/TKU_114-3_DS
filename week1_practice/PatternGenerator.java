86acd164f9807e88df92e258ad250a034d11a735
eden299 <eden950310@gmail.com>
2026-07-12 23:24:20 +0800

    Add week 1 practice

---
+++ week1_practice/PatternGenerator.java
@@ -0,0 +1,111 @@
+package week1_practice;
+
+import java.util.Scanner;
+
+public class PatternGenerator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            printMenu();
+            int choice = sc.nextInt();
+
+            if (choice == 0) {
+                System.out.println("程式已結束，謝謝使用！");
+                break;
+            }
+
+            switch (choice) {
+                case 1:
+                    printMultiplicationTable();
+                    break;
+
+                case 2:
+                    int triHeight = readPositiveInt(sc, "請輸入三角形高度: ");
+                    printTriangle(triHeight);
+                    break;
+
+                case 3:
+                    int revTriHeight = readPositiveInt(sc, "請輸入倒三角形高度: ");
+                    printReverseTriangle(revTriHeight);
+                    break;
+
+                case 4:
+                    int rows = readPositiveInt(sc, "請輸入列數: ");
+                    int cols = readPositiveInt(sc, "請輸入欄數: ");
+                    printNumberGrid(rows, cols);
+                    break;
+
+                default:
+                    System.out.println("無效的選項，請重新輸入。");
+                    break;
+            }
+        }
+
+        sc.close();
+    }
+
+    public static void printMenu() {
+        System.out.println("=== Pattern Menu ===");
+        System.out.println("1. 九九乘法表");
+        System.out.println("2. 正三角形星號");
+        System.out.println("3. 倒三角形星號");
+        System.out.println("4. 數字方格");
+        System.out.println("0. 離開");
+        System.out.print("請輸入選項: ");
+    }
+
+    public static int readPositiveInt(Scanner sc, String message) {
+        int value = 0;
+        while (true) {
+            System.out.print(message);
+            value = sc.nextInt();
+            if (value > 0) {
+                break;
+            } else {
+                System.out.println("錯誤！輸入數值必須大於 0，請重新輸入。");
+            }
+        }
+        return value;
+    }
+
+    public static void printMultiplicationTable() {
+        System.out.println("--- 九九乘法表 ---");
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.print(i + "x" + j + "=" + (i * j) + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printTriangle(int height) {
+        System.out.println("--- 正三角形 ---");
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printReverseTriangle(int height) {
+        System.out.println("--- 倒三角形 ---");
+        for (int i = height; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printNumberGrid(int rows, int cols) {
+        System.out.println("--- 數字方格 ---");
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
